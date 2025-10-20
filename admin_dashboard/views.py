a6a17b5ea7e036c477ae264fd9be149202900b0c
ziko278 <odekeziko@gmail.com>
2025-10-20 10:49:45 +0100

    h

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -35,7 +35,7 @@ def setup_test():
 
 def fix(request):
     alphabet = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'
-    staff_list = StaffModel.objects.filter(genfer='MALE')
+    staff_list = StaffModel.objects.filter(gender='MALE')
 
     for staff in staff_list:
         username = staff.staff_id.lower()
